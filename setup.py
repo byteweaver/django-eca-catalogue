f4dddf44b19b45f5ff42ad636644aea301ed15b5
akuryou <akuryou@akuryou.net>
2012-10-19 12:41:13 +0200

    init app with adjusted skeleton

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+import catalogue
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-eca-catalogue',
+    version=catalogue.__version__,
+    description='A reusable django app providing a generic product catalog for E-comerce projects',
+    long_description=read('README.md'),
+    license=read('LICENSE'),
+    author='byteweaver',
+    author_email='contact@byteweaver.net',
+    url='https://github.com/byteweaver/django-eca-catalogue',
+    packages=find_packages(),
+    install_requires=[
+        'django',
+    ],
+    tests_require=[
+        'django-nose',
+        'coverage',
+        'django-coverage',
+    ],
+    test_suite='catalogue.tests',
+)
