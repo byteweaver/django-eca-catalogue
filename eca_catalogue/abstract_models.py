42bf015083a98861628247be16b2f79087432719
akuryou <akuryou@byteweaver.net>
2012-10-26 11:15:28 +0200

    exctract name-slug-desc kombo to mixin

--- eca_catalogue/abstract_models.py
+++ eca_catalogue/abstract_models.py
@@ -4,11 +4,15 @@ from django.utils.translation import ugettext_lazy as _
 from treebeard.mp_tree import MP_Node
 
 
-class AbstractNestedProductCategory(MP_Node):
+class NSDMixin(models.Model):
     name = models.CharField(_("Name"), max_length=128)
     slug = models.SlugField(_("Slug"), max_length=128, unique=True)
     description = models.TextField(_("Description"), blank=True, null=True)
 
+    class Meta:
+        abstract = True
+
+class AbstractNestedProductCategory(MP_Node, NSDMixin):
     class Meta:
         abstract = True
         verbose_name = _("Nested product category")
