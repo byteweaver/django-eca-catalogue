1ec9b048b3f5bc17621baea5e01292b70156677d
akuryou <akuryou@byteweaver.net>
2012-10-26 11:48:49 +0200

    add AbstractProductCategoryAdmin

--- eca_catalogue/abstract_admin.py
+++ eca_catalogue/abstract_admin.py
@@ -1,5 +1,10 @@
+from django.contrib import admin
+
 from treebeard.admin import TreeAdmin
 
+
+class AbstractProductCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
 
 class AbstractNestedProductCategoryAdmin(TreeAdmin):
     prepopulated_fields = {"slug": ("name",)}
