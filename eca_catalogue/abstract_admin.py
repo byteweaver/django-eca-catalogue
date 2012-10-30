607a90d9e683547dab2a5619ab52d5da68252f1a
akuryou <akuryou@byteweaver.net>
2012-10-30 16:32:14 +0100

    add admin for SellingPoint

--- eca_catalogue/abstract_admin.py
+++ eca_catalogue/abstract_admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import TextInput
 
 from treebeard.admin import TreeAdmin
+
+from abstract_models import AbstractSellingPoint
 
 
 class AbstractProductCategoryAdmin(admin.ModelAdmin):
@@ -12,4 +16,18 @@ class AbstractNestedProductCategoryAdmin(TreeAdmin):
 class AbstractProductAdmin(admin.ModelAdmin):
     list_display = ['item_number', 'name',]
     prepopulated_fields = {"slug": ("name",)}
+
+class AbstractSellingPointAdmin(admin.ModelAdmin):
+    """name your foreign key to product model "product" in order to fully utilize this class"""
+    search_fields = ['text', 'product__name', 'product__item_number',]
+    list_display = ['product', 'text',]
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '150'})},
+    }
+
+class AbstractSellingPointInline(admin.StackedInline):
+    extra = 0
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '150'})},
+    }
 
