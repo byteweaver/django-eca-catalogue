6afec947d232da7c6c93fb8a00789d8fea1775aa
akuryou <akuryou@byteweaver.net>
2012-11-09 15:42:17 +0100

    remove AbstractSellingPointInline

--- eca_catalogue/abstract_admin.py
+++ eca_catalogue/abstract_admin.py
@@ -47,14 +47,6 @@ class AbstractSellingPointAdmin(admin.ModelAdmin):
     }
 
 
-class AbstractSellingPointInline(admin.StackedInline):
-    """set model=YourSellingPointModel in order to get this working"""
-    extra = 0
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size': '150'})},
-    }
-
-
 class AbstractWashingInstructionAdmin(admin.ModelAdmin):
     list_display = ['text', 'render_icon',]
     search_fields = ['text',]
