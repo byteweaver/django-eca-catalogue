b121b5e28c92a692a4f9f005002225f52de46e11
akuryou <akuryou@byteweaver.net>
2013-02-11 18:08:10 +0100

    move SellingPointAdmin to text module

---
+++ eca_catalogue/text/abstract_admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.db import models
+
+from abstract_models import AbstractSellingPoint
+
+
+class AbstractSellingPointAdmin(admin.ModelAdmin):
+    """name your foreign key to product model "product" in order to fully utilize this class"""
+    search_fields = ['text', 'product__name', 'product__item_number',]
+    list_display = ['product', 'text',]
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '150'})},
+    }
+
