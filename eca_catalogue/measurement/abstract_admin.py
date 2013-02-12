048458ac21babaa5d6023b0207511dcc10072e49
akuryou <akuryou@byteweaver.net>
2013-02-12 17:07:42 +0100

    add missing imports

--- eca_catalogue/measurement/abstract_admin.py
+++ eca_catalogue/measurement/abstract_admin.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+
+
 # fieldset for PackageMeasurementMixin, use like: fieldsets.append(package_measurement_fieldset)
 package_measurement_fieldset = [_("Package measurement"), {
     'fields': ['length', 'width', 'height', 'weight']
--- eca_catalogue/text/abstract_admin.py
+++ eca_catalogue/text/abstract_admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db import models
+from django.forms.widgets import TextInput
 
 
 class AbstractSellingPointAdmin(admin.ModelAdmin):
