451ff611898019cb44ca670d431895a3c646f2ad
akuryou <akuryou@byteweaver.net>
2013-02-12 09:22:31 +0100

    add test models

---
+++ eca_catalogue/categorization/tests/models.py
@@ -0,0 +1,10 @@
+from eca_catalogue.categorization.abstract_models import *
+
+
+class ProductCategory(AbstractProductCategory):
+    pass
+
+
+class NestedProductCategory(AbstractNestedProductCategory):
+    pass
+
