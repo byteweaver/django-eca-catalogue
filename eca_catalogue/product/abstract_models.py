8f8b993cf814e90d138cab7a8fc59c3b52bd3078
akuryou <akuryou@byteweaver.net>
2013-02-12 11:21:55 +0100

    change field order

--- eca_catalogue/product/abstract_models.py
+++ eca_catalogue/product/abstract_models.py
@@ -3,15 +3,15 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class AbstractProduct(models.Model):
+    item_number = models.CharField(_("Item number"), max_length=255, unique=True)
     name = models.CharField(_("Name"), max_length=128)
     description = models.TextField(_("Description"), blank=True, null=True)
-    item_number = models.CharField(_("Item number"), max_length=255, unique=True)
 
     class Meta:
         abstract = True
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
-        ordering = ['name']
+        ordering = ['item_number']
 
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.item_number)
