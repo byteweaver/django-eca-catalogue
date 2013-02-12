920c2d1b048fc2798ceff86077b1da7e9ae2fcb2
akuryou <akuryou@byteweaver.net>
2013-02-12 17:08:11 +0100

    set product to raw_id_field in AbstractSellingPointAdmin

--- eca_catalogue/text/abstract_admin.py
+++ eca_catalogue/text/abstract_admin.py
@@ -7,6 +7,7 @@ class AbstractSellingPointAdmin(admin.ModelAdmin):
     """name your foreign key to product model "product" in order to fully utilize this class"""
     search_fields = ['text', 'product__name', 'product__item_number',]
     list_display = ['product', 'text',]
+    raw_id_fields = ['product',]
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size': '150'})},
     }
